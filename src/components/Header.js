a8babfda9d8f384c22b5ebaf4b2183f8cadde4b3
sannimdev <crontables@gmail.com>
2025-02-20 16:02:43 +0900

    refactor: 헤더 컴포넌트 props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-export function createHeader() {
+export function createHeader(title = "점심 뭐 먹지") {
   const header = document.createElement("header");
   header.classList.add("gnb");
   header.innerHTML = /*html*/ `
-  <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+  <h1 class="gnb__title text-title">${title}</h1>
   <button type="button" class="gnb__button" aria-label="음식점 추가">
     <img src="./public/assets/add-button.png" alt="음식점 추가" />
   </button>`;
