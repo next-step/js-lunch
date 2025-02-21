c2f9b8d5dfb805dc4199a4db2a283709917d324f
IMHOJEONG <39ghwjd@naver.com>
2025-02-21 13:24:30 +0900

    feat: add func 1 - header 컴포넌트 작성

---
+++ cypress/e2e/header.cy.js
@@ -0,0 +1,10 @@
+describe("Header Component", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+  });
+
+  it("헤더 컴포넌트가 잘 로드되는지 확인한다.", () => {
+    cy.get("header").should("exist");
+  });
+
+});
---
+++ src/layout/Header.js
@@ -0,0 +1,3 @@
+const Header = () => /* html */ `<header>점심 뭐먹지</header>`;
+
+export default Header;
