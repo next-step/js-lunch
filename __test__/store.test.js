94ac687519e3c34d107c671f305bc892533c44ce
mandarin-sep <vheh0901@gmail.com>
2025-02-22 17:34:41 +0900

    feat: 테스트 코드 작성

--- __test__/store.test.js
+++ __test__/store.test.js
@@ -1,1 +1,32 @@
-describe();
+import data from "../example.json";
+
+describe("음식점 목록이 있을때", () => {
+  const storeList = data.storeList;
+
+  test("음식점 목록이 거리순으로 오름차순으로 정렬된다.", () => {
+    const storeNameLsit = storeList
+      .slice()
+      .sort((a, b) => a.distance - b.distance)
+      .map((item) => item.name);
+
+    expect(storeNameLsit.join(", ")).toBe(
+      "이태리 키친, 호아빈 삼성점, 피양콩 할머니, 친친, 잇쇼우"
+    );
+  });
+  test("음식점 목록이 이름순순으로 오름차순으로 정렬된다.", () => {
+    const storeNameLsit = storeList
+      .sort((a, b) => a.name - b.name)
+      .map((item) => item.name);
+
+    expect(storeNameLsit.join(", ")).toBe(
+      "피양콩 할머니, 친친, 잇쇼우, 이태리 키친, 호아빈 삼성점"
+    );
+  });
+  test("음식점 목록이 카테고리 별로 필터링된다.", () => {
+    const storeNameLsit = storeList
+      .filter((item) => item.category === "한식")
+      .map((item) => item.name);
+
+    expect(storeNameLsit.join(", ")).toBe("피양콩 할머니");
+  });
+});
