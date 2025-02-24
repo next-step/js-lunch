bcc582ba89a40fdd831ca29e40feeccc3d8bdb09
minji Kong <minzyaaaaaa@gmail.com>
2025-02-25 00:20:26 +0900

    Feat: 헤더 컴포넌트

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
-import image from "../public/assets/favorite-icon-filled.png";
+import { createHeader } from "./components/header";
 
-console.log("npm run dev 명령어를 통해 점심 뭐 먹지 미션을 시작하세요");
-console.log(
-  "%c ___       ___  ___  ________   ________  ___  ___     \n" +
-    "|\\  \\     |\\  \\|\\  \\|\\   ___  \\|\\   ____\\|\\  \\|\\  \\    \n" +
-    "\\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\___|\\ \\  \\\\\\  \\   \n" +
-    " \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\    \\ \\   __  \\  \n" +
-    "  \\ \\  \\____\\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\____\\ \\  \\ \\  \\ \n" +
-    "   \\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\ \\__\\ \\__\\\n" +
-    "    \\|_______|\\|_______|\\|__| \\|__|\\|_______|\\|__|\\|__|",
-  "color: #d81b60; font-size: 14px; font-weight: bold;"
-);
-
-// 자바스크립트 코드에서 이미지 리소스 로드 테스트
-// index.html 파일의 html 구조를 수정하셔도 됩니다.
-addEventListener("load", () => {
+addEventListener("DOMContentLoaded", () => {
   const app = document.querySelector("#app");
-  const buttonImage = document.createElement("img");
-  buttonImage.src = image;
 
-  if (app) {
-    app.appendChild(buttonImage);
-  }
+  const header = createHeader();
+  app.prepend(header);
 });
