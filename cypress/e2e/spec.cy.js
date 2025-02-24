25647c0f725a7b2a208666fbd9ae30aa4ad72aba
Terry <kdjtiger@gmail.com>
2025-02-24 23:52:09 +0900

    feat: header 컴포넌트 추가

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:5173/')
-  })
-})
+describe("헤더 컴포넌트", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
+
+  it("헤더 컴포넌트 로딩 확인", () => {
+    cy.get(".gnb").should("exist");
+    cy.get(".gnb__title").contains("점심 뭐 먹지");
+  });
+});
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export const createFragment = () => document.createDocumentFragment();
