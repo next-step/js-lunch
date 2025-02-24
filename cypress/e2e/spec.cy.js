68a4346f86a60a1b1aa9783eacf29c955be2b68d
Terry <kdjtiger@gmail.com>
2025-02-25 00:18:46 +0900

    feat: 정적 컨텐츠 로드

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,3 +8,38 @@ describe("헤더 컴포넌트", () => {
     cy.get(".gnb__title").contains("점심 뭐 먹지");
   });
 });
+
+describe("정적 콘텐츠 테스트", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
+  it("카테고리 필터가 올바르게 렌더 된다.", () => {
+    cy.get("#category-filter").should("exist").and("have.value", "전체");
+
+    const categories = [
+      "전체",
+      "한식",
+      "중식",
+      "일식",
+      "양식",
+      "아시안",
+      "기타",
+    ];
+
+    categories.forEach((category) => {
+      cy.get("#category-filter")
+        .find("option")
+        .contains(category)
+        .should("exist");
+    });
+  });
+
+  it("정렬 필터가 올바르게 렌더 된다.", () => {
+    cy.get("#sorting-filter").should("exist").and("have.value", "name");
+
+    const sortingOptions = ["이름순", "거리순"];
+    sortingOptions.forEach((option) =>
+      cy.get("#sorting-filter").find("option").contains(option).should("exist"),
+    );
+  });
+});
