0a5092a809635fac9143eeb83cad435e1884e23c
Terry <kdjtiger@gmail.com>
2025-02-25 22:37:08 +0900

    refactor: 레스토랑 섹션 초기 렌더링 시 정렬 기능 추가

--- src/components/RestaurantListSection.js
+++ src/components/RestaurantListSection.js
@@ -1,11 +1,22 @@
+import { sortingBy } from '../utils/sorting';
 import { createRestaurantList } from "./RestaurantList";
 
-export const createRestaurantListSection = () => {
+export const createRestaurantListSection = (restaurants) => {
   const restaurantListSection = document.createElement("section");
   restaurantListSection.classList.add("restaurant-list-container");
 
-  const restaurantList = createRestaurantList();
+  const restaurantList = renderRestaurantList(restaurants);
   restaurantListSection.appendChild(restaurantList);
   
   return restaurantListSection;
+};
+
+const renderRestaurantList = (restaurants) => {
+  const sortingFilter = document.getElementById("sorting-filter");
+  const selectedSort = sortingFilter.value; 
+
+  const sortedRestaurantData = sortingBy(selectedSort, restaurants); 
+  const restaurantList = createRestaurantList(sortedRestaurantData); 
+
+  return restaurantList;
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createHeader } from "./components/Header";
 import { createRestaurantListSection } from "./components/RestaurantListSection";
+import { restaurantData } from './data/restaurantData';
 import { addEvent } from "./utils/addEvent";
 
 addEventListener("load", () => {
@@ -8,7 +9,7 @@ addEventListener("load", () => {
   body.prepend(header);
 
   const main = document.querySelector("main");
-  const restaurantListSection = createRestaurantListSection();
+  const restaurantListSection = createRestaurantListSection(restaurantData);
   main.appendChild(restaurantListSection);
 
   addEvent();
