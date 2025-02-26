9a0a252ddbdfa812df51d72e7b45aa685ee2a940
etff <wish2rich@gmail.com>
2025-02-27 00:23:55 +0900

    test: 모달 테스트 케이스 추가

--- cypress/e2e/modal-component-spec.cy.js
+++ cypress/e2e/modal-component-spec.cy.js
@@ -1,10 +1,14 @@
-describe("모달 컴포넌트 테스트", () => {
+describe("Modal 컴포넌트 테스트", () => {
   beforeEach(() => {
     cy.visit("localhost:5173");
   });
 
-  it("음식점 버튼을 누르면 모달창이 열려야 한다", () => {
+  it("모달은 처음에 닫혀 있어야 한다", () => {
+    cy.get(".modal").should("not.have.class", "modal--open");
+  });
+
+  it("헤더의 음식점 추가 버튼 클릭 시 모달이 열려야 한다", () => {
     cy.get(".gnb__button").click();
-    cy.get(".modal.modal--open").should("exist");
+    cy.get(".modal").should("have.class", "modal--open");
   });
 });
