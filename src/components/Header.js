c774ae76e85374339dbda412ab9de08befccb175
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-27 01:46:05 +0900

    feat: index.js 생성할때 restaurantListInstance, modal 생성하도록 생성자 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function createHeader(name) {
 
   header.innerHTML = /*html*/ `
         <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-        <button type="button" class="gnb__button" aria-label="음식점 추가">
+        <button type="button" id= "gnb__button", class="gnb__button" aria-label="음식점 추가">
           <img src="./templates/add-button.png" alt="음식점 추가">
         </button>
       `;
--- src/index.js
+++ src/index.js
@@ -3,24 +3,52 @@ import { filterRestaurant, restaurantRender } from "./components/Restaurant.js";
 import { categoryRender } from "./components/Category.js";
 import RestaurantListModel from "./domain/RestaurantListModel.js";
 import CategoryListModel from "./domain/CategoryListModel.js";
+import Modal from "./Modal.js";
 
 class Index {
   #restaurantListInstance;
+  #modal;
+
+  constructor() {
+    this.#restaurantListInstance = new RestaurantListModel();
+    this.#modal = new Modal(this.#restaurantListInstance);
+  }
 
   init() {
     addEventListener("load", () => {
       headerRender("점심 뭐 먹지");
-      categoryRender(new CategoryListModel());
-      this.#restaurantListInstance = new RestaurantListModel();
+      categoryRender("전체", "#category-filter", new CategoryListModel());
       restaurantRender(this.#restaurantListInstance);
-    });
 
+      this.#clickEvent();
+    });
+  }
+
+  get restaurantListInstance() {
+    return this.#restaurantListInstance;
+  }
+
+  #clickEvent() {
+    this.#gnbButtonClick();
+    this.#categoryFilterChange();
+    this.#sortFilterChange();
+  }
+
+  #gnbButtonClick() {
+    document.getElementById("gnb__button").addEventListener("click", () => {
+      this.#modal.openModal();
+    });
+  }
+
+  #categoryFilterChange() {
     document
       .getElementById("category-filter")
       .addEventListener("change", () => {
         filterRestaurant(this.#restaurantListInstance);
       });
+  }
 
+  #sortFilterChange() {
     document.getElementById("sorting-filter").addEventListener("change", () => {
       filterRestaurant(this.#restaurantListInstance);
     });
