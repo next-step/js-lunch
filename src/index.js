ceb70838beeb033cfe28990664dea37252525a4d
minji Kong <minzyaaaaaa@gmail.com>
2025-02-27 17:36:09 +0900

    Feat: 레스토랑 리스트 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createHeader } from "./components/header";
 import { createSelect } from "./components/select";
+import { createList } from "./components/list";
 
 const createCategoryFilter = () => {
   return createSelect({
@@ -32,17 +33,84 @@ const createSortingFilter = () => {
   });
 };
 
-addEventListener("DOMContentLoaded", () => {
-  const app = document.querySelector("#app");
-
-  const header = createHeader();
-  app.prepend(header);
-
+const createRestaurantFilter = () => {
   const categoryFilter = createCategoryFilter();
   const sortingFilter = createSortingFilter();
 
   const restaurantFilterContainer = document.createElement("section");
   restaurantFilterContainer.classList.add("restaurant-filter-container");
   restaurantFilterContainer.append(categoryFilter, sortingFilter);
-  app.append(restaurantFilterContainer);
+
+  return restaurantFilterContainer;
+};
+
+const getRestaurantList = async () => {
+  return await fetch("../__mock__/db.json").then((res) => res.json());
+};
+
+const createRestaurantList = (data) => {
+  const restaurantListContainer = document.createElement("section");
+  restaurantListContainer.classList.add("restaurant-list-container");
+
+  const restaurantList = document.createElement("ul");
+  restaurantList.classList.add("restaurant-list");
+  restaurantListContainer.append(restaurantList);
+
+  const restaurantElements = data.map((restaurant) => {
+    const restaurantElement = document.createElement("li");
+    restaurantElement.classList.add("restaurant");
+
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("restaurant__category");
+    restaurantElement.append(imageContainer);
+
+    const categoryImage = document.createElement("img");
+    const imagePaths = {
+      한식: "category-korean.png",
+      중식: "category-chinese.png",
+      일식: "category-japanese.png",
+      양식: "category-western.png",
+      아시안: "category-asian.png",
+      기타: "category-etc.png",
+    };
+
+    categoryImage.src = `../public/assets/${imagePaths[restaurant.category]}`;
+    categoryImage.alt = restaurant.category;
+    categoryImage.classList.add("category-icon");
+    imageContainer.append(categoryImage);
+
+    const infoContainer = document.createElement("div");
+    infoContainer.classList.add("restaurant__info");
+    restaurantElement.append(infoContainer);
+
+    const name = document.createElement("h3");
+    name.classList.add("restaurant__name", "text-subtitle");
+    name.textContent = restaurant.name;
+    infoContainer.append(name);
+
+    const distance = document.createElement("span");
+    distance.classList.add("restaurant__distance", "text-body");
+    distance.textContent = `캠퍼스로부터 ${restaurant.distanceMinutes}분 내`;
+    infoContainer.append(distance);
+
+    const description = document.createElement("p");
+    description.classList.add("restaurant__description", "text-body");
+    description.textContent = restaurant.description;
+    infoContainer.append(description);
+
+    return restaurantElement;
+  });
+
+  restaurantList.append(...restaurantElements);
+  return restaurantListContainer;
+};
+
+addEventListener("DOMContentLoaded", async () => {
+  const app = document.querySelector("#app");
+
+  app.prepend(createHeader());
+  app.append(createRestaurantFilter());
+
+  const restaurantList = await getRestaurantList();
+  app.append(createRestaurantList(restaurantList));
 });
