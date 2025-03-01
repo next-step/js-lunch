d085dc04c74bac908ce5947730bdcb0e0219361e
etff <wish2rich@gmail.com>
2025-03-01 16:17:37 +0900

    fix: 정렬 기준 test fail 수정

--- cypress/e2e/restaurant-item-component-spec.cy.js
+++ cypress/e2e/restaurant-item-component-spec.cy.js
@@ -11,61 +11,24 @@ describe("음식점 항목 컴포넌트 테스트", () => {
       .within(() => {
         cy.get(".restaurant__name.text-subtitle").should(
           "contain",
-          "피양콩할마니",
+          "도스타코스 선릉점",
         );
-        cy.get(".restaurant__distance.text-body").should(
-          "contain",
-          "캠퍼스부터 10분 내",
-        );
-        cy.get(".restaurant__description.text-body").should(
-          "contain",
-          "평양 출신의 할머니",
-        );
-        cy.get(".restaurant__category img")
-          .should("have.attr", "alt", "한식")
-          .and("have.attr", "src")
-          .should("include", "category-korean.png");
-      });
-
-    cy.get(".restaurant-list .restaurant")
-      .eq(1)
-      .within(() => {
-        cy.get(".restaurant__name.text-subtitle").should("contain", "친친");
         cy.get(".restaurant__distance.text-body").should(
           "contain",
           "캠퍼스부터 5분 내",
         );
         cy.get(".restaurant__description.text-body").should(
           "contain",
-          "정통 중식의 세계",
+          "멕시칸 캐주얼 그릴",
         );
         cy.get(".restaurant__category img")
-          .should("have.attr", "alt", "중식")
+          .should("have.attr", "alt", "기타")
           .and("have.attr", "src")
-          .should("include", "category-chinese.png");
+          .should("include", "category-etc.png");
       });
 
     cy.get(".restaurant-list .restaurant")
-      .eq(2)
-      .within(() => {
-        cy.get(".restaurant__name.text-subtitle").should("contain", "잇쇼우");
-        cy.get(".restaurant__distance.text-body").should(
-          "contain",
-          "캠퍼스부터 10분 내",
-        );
-        cy.get(".restaurant__description.text-body").should(
-          "contain",
-          "정통 자가제면 사누끼 우동",
-        );
-        cy.get(".restaurant__category img")
-          .should("have.attr", "alt", "일식")
-          .and("have.attr", "src")
-          .should("include", "category-japanese.png");
-      });
-
-    // 네 번째 아이템: 이태리키친 (양식, 20분)
-    cy.get(".restaurant-list .restaurant")
-      .eq(3)
+      .eq(1)
       .within(() => {
         cy.get(".restaurant__name.text-subtitle").should(
           "contain",
@@ -86,7 +49,64 @@ describe("음식점 항목 컴포넌트 테스트", () => {
       });
 
     cy.get(".restaurant-list .restaurant")
+      .eq(2)
+      .within(() => {
+        cy.get(".restaurant__name.text-subtitle").should("contain", "잇쇼우");
+        cy.get(".restaurant__distance.text-body").should(
+          "contain",
+          "캠퍼스부터 10분 내",
+        );
+        cy.get(".restaurant__description.text-body").should(
+          "contain",
+          "정통 자가제면 사누끼 우동",
+        );
+        cy.get(".restaurant__category img")
+          .should("have.attr", "alt", "일식")
+          .and("have.attr", "src")
+          .should("include", "category-japanese.png");
+      });
+
+    cy.get(".restaurant-list .restaurant")
+      .eq(3)
+      .within(() => {
+        cy.get(".restaurant__name.text-subtitle").should("contain", "친친");
+        cy.get(".restaurant__distance.text-body").should(
+          "contain",
+          "캠퍼스부터 5분 내",
+        );
+        cy.get(".restaurant__description.text-body").should(
+          "contain",
+          "정통 중식의 세계",
+        );
+        cy.get(".restaurant__category img")
+          .should("have.attr", "alt", "중식")
+          .and("have.attr", "src")
+          .should("include", "category-chinese.png");
+      });
+
+    cy.get(".restaurant-list .restaurant")
       .eq(4)
+      .within(() => {
+        cy.get(".restaurant__name.text-subtitle").should(
+          "contain",
+          "피양콩할마니",
+        );
+        cy.get(".restaurant__distance.text-body").should(
+          "contain",
+          "캠퍼스부터 10분 내",
+        );
+        cy.get(".restaurant__description.text-body").should(
+          "contain",
+          "평양 출신의 할머니",
+        );
+        cy.get(".restaurant__category img")
+          .should("have.attr", "alt", "한식")
+          .and("have.attr", "src")
+          .should("include", "category-korean.png");
+      });
+
+    cy.get(".restaurant-list .restaurant")
+      .eq(5)
       .within(() => {
         cy.get(".restaurant__name.text-subtitle").should(
           "contain",
@@ -104,27 +124,6 @@ describe("음식점 항목 컴포넌트 테스트", () => {
           .should("have.attr", "alt", "아시안")
           .and("have.attr", "src")
           .should("include", "category-chinese.png");
-      });
-
-    cy.get(".restaurant-list .restaurant")
-      .eq(5)
-      .within(() => {
-        cy.get(".restaurant__name.text-subtitle").should(
-          "contain",
-          "도스타코스 선릉점",
-        );
-        cy.get(".restaurant__distance.text-body").should(
-          "contain",
-          "캠퍼스부터 5분 내",
-        );
-        cy.get(".restaurant__description.text-body").should(
-          "contain",
-          "멕시칸 캐주얼 그릴",
-        );
-        cy.get(".restaurant__category img")
-          .should("have.attr", "alt", "기타")
-          .and("have.attr", "src")
-          .should("include", "category-etc.png");
       });
   });
 });
