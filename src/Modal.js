a837a3b0b4b4b04f1b42008f87b7d08c2ff0a534
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-01 23:07:00 +0900

    refactor: 음식점 제출 버튼 클릭 메소드를 분리하여 가독성 향상

--- src/Modal.js
+++ src/Modal.js
@@ -21,7 +21,7 @@ class Modal {
 
   #clickEvent() {
     this.#cancelButtonClick();
-    this.#submitButtionClick();
+    this.#submitButtonClick();
   }
 
   #cancelButtonClick() {
@@ -31,27 +31,50 @@ class Modal {
     });
   }
 
-  #submitButtionClick() {
+  #submitButtonClick() {
     document
       .querySelector(".button--primary")
       .addEventListener("click", (event) => {
-        const category = this.#validationCategory();
-        const name = this.#validationName();
-        const time = this.#validationTime();
-        const description = document.getElementById("description").value;
-        const link = document.getElementById("link").value;
-
-        if (category && name && time) {
-          this.#restaurantListInstance.addRestaurant(
-            category,
-            name,
-            time,
-            description,
-            link
-          );
-          this.#closeModal();
-        }
+        this.#addRestaurant();
       });
+  }
+
+  #addRestaurant() {
+    const restaurantData = this.#validateRestaurantData();
+
+    if (!restaurantData) {
+      return;
+    }
+
+    this.#restaurantListInstance.addRestaurant(
+      restaurantData.category,
+      restaurantData.name,
+      restaurantData.time,
+      restaurantData.description,
+      restaurantData.link
+    );
+
+    this.#closeModal();
+  }
+
+  #validateRestaurantData() {
+    const category = this.#validationCategory();
+    const name = this.#validationName();
+    const time = this.#validationTime();
+    const description = document.getElementById("description").value;
+    const link = document.getElementById("link").value;
+
+    if (!category || !name || !time) {
+      return null;
+    }
+
+    return {
+      category,
+      name,
+      time,
+      description,
+      link,
+    };
   }
 
   #closeModal() {
