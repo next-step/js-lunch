8392dc739c48333fcd67e55d84823069dc03f49a
Terry <kdjtiger@gmail.com>
2025-03-19 23:32:58 +0900

    refactor: fragment util 폴더에서 삭제

--- src/components/restaurantList/RestaurantList.js
+++ src/components/restaurantList/RestaurantList.js
@@ -1,12 +1,11 @@
 import { restaurantData } from "../../data/restaurantData";
-import { createFragment } from '../../utils';
 import { createRestaurantListItem } from "./RestaurantListItem";
 
 export const createRestaurantList = (restaurants = restaurantData) => {
   const restaurantList = document.createElement("ul");
   restaurantList.classList.add("restaurant-list");
 
-  const fragment = createFragment();
+  const fragment = document.createDocumentFragment();
 
   restaurants.forEach((item) => {
     const listItem = createRestaurantListItem({
