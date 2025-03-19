7d56fc140c02d7fcbac3bd477c545e05d1671875
Terry <kdjtiger@gmail.com>
2025-03-19 23:06:58 +0900

    feat: 음식점 추가 기능

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,3 +1,5 @@
+import { restaurantManager } from "../../domain/restaurant/restaurantManager";
+
 class Modal {
   #container;
   #backdrop;
@@ -14,6 +16,31 @@ class Modal {
 
     this.#closeButton = document.querySelector(".close-modal-btn");
     this.#closeButton.addEventListener("click", () => this.toggle());
+
+    this.#addButton = document.querySelector(".add-restaurant-btn");
+    this.#addButton.addEventListener("click", (event) => this.#submit(event));
+  }
+
+  #submit(event) {
+    event.preventDefault();
+    
+    const form = document.querySelector(".restaurant-form");
+    const category = form.querySelector("#category").value;
+    const name = form.querySelector("#name").value;
+    const distance = form.querySelector("#distance").value;
+    const description = form.querySelector("#description").value;
+    const link = form.querySelector("#link").value;
+
+    restaurantManager.addRestaurant({
+      category,
+      name,
+      distance,
+      description,
+      link,
+    });
+
+    restaurantManager.renderRestaurantList();
+    this.toggle();
   }
 
   toggle() {
@@ -21,11 +48,11 @@ class Modal {
     this.#container.classList.toggle("modal--open", this.#isOpen);
 
     if (!this.#isOpen) {
-      this.reset();
+      this.#reset();
     }
   }
 
-  reset() {
+  #reset() {
     const form = this.#container.querySelector("form");
     form?.reset();
   }
