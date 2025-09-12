0f5a62f9fd5d158b3d18b7532426e943a775a186
강현석 <kekemusa37@gmail.com>
2025-09-12 22:54:45 +0900

    feat: 카테고리 필터가 가능하도록 합니다.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-import image from "../public/assets/favorite-icon-filled.png";
-
-console.log("npm run dev 명령어를 통해 점심 뭐 먹지 미션을 시작하세요");
-console.log(
-  "%c ___       ___  ___  ________   ________  ___  ___     \n" +
-    "|\\  \\     |\\  \\|\\  \\|\\   ___  \\|\\   ____\\|\\  \\|\\  \\    \n" +
-    "\\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\___|\\ \\  \\\\\\  \\   \n" +
-    " \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\    \\ \\   __  \\  \n" +
-    "  \\ \\  \\____\\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\____\\ \\  \\ \\  \\ \n" +
-    "   \\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\ \\__\\ \\__\\\n" +
-    "    \\|_______|\\|_______|\\|__| \\|__|\\|_______|\\|__|\\|__|",
-  "color: #d81b60; font-size: 14px; font-weight: bold;"
-);
-
-// 자바스크립트 코드에서 이미지 리소스 로드 테스트
-// index.html 파일의 html 구조를 수정하셔도 됩니다.
 addEventListener("load", () => {
-  const app = document.querySelector("#app");
-  const buttonImage = document.createElement("img");
-  buttonImage.src = image;
+  const restaurants = document.querySelectorAll(".restaurant-list .restaurant");
+  const categoryFilter = document.getElementById("category-filter");
 
-  if (app) {
-    app.appendChild(buttonImage);
-  }
+  categoryFilter.addEventListener("change", function () {
+    const selectedCategory = categoryFilter.value;
+    restaurants.forEach((li) => {
+      const image = li.querySelector("img.category-icon");
+      if (selectedCategory === "전체" || image.alt === selectedCategory) {
+        li.style.display = "";
+      } else {
+        li.style.display = "none";
+      }
+    });
+  });
 });
