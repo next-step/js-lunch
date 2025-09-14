99b8aef607277ec7cdae90ca9ea8ed940d2feca7
Seung-wan <tmddhks0104@gmail.com>
2025-09-14 12:05:45 +0900

    배포 에러 수정

--- src/components/gnb.js
+++ src/components/gnb.js
@@ -4,7 +4,7 @@ export class Gnb {
       <header class="gnb">
         <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
         <button type="button" class="gnb__button" aria-label="음식점 추가">
-          <img src="/src/assets/add-button.png" alt="음식점 추가" />
+          <img src="src/assets/add-button.png" alt="음식점 추가" />
         </button>
       </header>
     `;
