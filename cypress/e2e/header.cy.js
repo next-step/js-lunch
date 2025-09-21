8c80a095b28b14fc6c1c0ef236779aeafbe3f7e0
강현석 <kekemusa37@gmail.com>
2025-09-21 11:44:57 +0900

    feat: 추가 버튼이 노출되도록 합니다

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -3,9 +3,14 @@ describe("Header", () => {
     cy.visit("http://localhost:5173");
   });
   it("영역이 노출되는지 확인합니다.", () => {
-    cy.get("header.gnb").should("exist");    
+    cy.get("header.gnb").should("exist");
+  });
+  it("추가 버튼이 노출되는지 확인합니다.", () => {
+    cy.get("header.gnb .gnb__add-button").should("exist");
   });
   it("타이틀이 노출되는지 확인합니다.", () => {
-    cy.get("header.gnb .gnb__title").should("exist").and("have.text", "점심 뭐 먹지");
-  })
+    cy.get("header.gnb .gnb__title")
+      .should("exist")
+      .and("have.text", "점심 뭐 먹지");
+  });
 });
