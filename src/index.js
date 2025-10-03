2bb81922912dc955f384c30f89bd21156a9474a3
강현석 <kekemusa37@gmail.com>
2025-10-03 22:37:00 +0900

    feat: 카테고리, 이름, 거리, 설명, 참고 링크를 확인할 수 있습니다.

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const restaurantData = [
     name: "도스타코스 선릉점",
     distance: 5,
     description: "맥시칸 캐주얼 그릴",
+    link: "https://naver.me/Gn0yLQ8K",
   },
   {
     icon: westernCategoryIcon,
@@ -27,6 +28,7 @@ const restaurantData = [
     name: "이태리키친",
     distance: 20,
     description: "늘 변화를 추구하는 이태리키친입니다.",
+    link: "https://naver.me/5huapW2k",
   },
   {
     icon: japaneseCategoryIcon,
@@ -35,6 +37,7 @@ const restaurantData = [
     distance: 10,
     description:
       "잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은\n정성을 이길 수 없다는 신념으로 모든 음식에 최선을 다하는\n잇쇼우는 고객 한분 한분께 최선을 다하겠습니다",
+    link: "https://naver.me/FLyTJ4dC",
   },
   {
     icon: chineseCategoryIcon,
@@ -43,6 +46,7 @@ const restaurantData = [
     distance: 5,
     description:
       "Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은 맛과\n정성으로 정통 중식의 세계를 펼쳐갑니다",
+    link: "https://naver.me/FV7Y4RTm",
   },
   {
     icon: koreanCategoryIcon,
@@ -51,6 +55,7 @@ const restaurantData = [
     distance: 10,
     description:
       "평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩\n할마니. 두부를 빼지 않은 되비지를 맛볼 수 있는 곳으로, ‘피양’은\n평안도 사투리로 ‘평양’을 의미한다. 딸과 함께 운영하는 이곳에선\n맷돌로 직접 간 콩만을 사용하며, 일체의 조미료를 넣지 않은\n건강식을 선보인다. 콩비지와 피양 만두가 이곳의 대표 메뉴지만,\n할머니가 옛날 방식을 고수하며 만들어내는 비지전골 또한 이 집의\n역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이 먹고 싶은\n만큼 덜어 먹을 수 있게 준비돼 있다.",
+    link: "https://naver.me/5Rh0ttMw",
   },
   {
     icon: asianCategoryIcon,
@@ -58,6 +63,7 @@ const restaurantData = [
     name: "호아빈 삼성점",
     distance: 15,
     description: "푸짐한 양에 국물이 일품인 쌀국수",
+    link: "https://naver.me/5WOQLjn6",
   },
 ];
 
@@ -108,6 +114,7 @@ function renderList(list) {
   list.forEach((restaurant) => {
     const li = document.createElement("li");
     li.className = "restaurant";
+    li.addEventListener("click", showRestaurantDetailModal);
 
     const categoryDiv = document.createElement("div");
     categoryDiv.className = "restaurant__category";
@@ -121,6 +128,9 @@ function renderList(list) {
     const infoDiv = document.createElement("div");
     infoDiv.className = "restaurant__info";
 
+    const title = document.createElement("div");
+    title.className = "restaurant__title";
+
     const name = document.createElement("h3");
     name.className = "restaurant__name text-subtitle";
     name.textContent = restaurant.name;
@@ -133,9 +143,14 @@ function renderList(list) {
     description.className = "restaurant__description text-body";
     description.textContent = restaurant.description;
 
-    infoDiv.appendChild(name);
-    infoDiv.appendChild(distance);
-    infoDiv.appendChild(description);
+    li.dataset.icon = restaurant.icon;
+    li.dataset.name = restaurant.name;
+    li.dataset.distance = restaurant.distance;
+    li.dataset.description = restaurant.description;
+    li.dataset.link = restaurant.link;
+
+    title.append(name, distance);
+    infoDiv.append(title, description);
     li.appendChild(infoDiv);
 
     fragment.appendChild(li);
@@ -339,6 +354,70 @@ function checkRequiredForms() {
   }
 
   return true;
+}
+
+function showRestaurantDetailModal(event) {
+  const target = event.currentTarget;
+  const restaurant = {
+    icon: target.dataset.icon,
+    category: target.dataset.category,
+    name: target.dataset.name,
+    distance: target.dataset.distance,
+    description: target.dataset.description,
+    link: target.dataset.link,
+  };
+  const prevModal = document.querySelector(".modal");
+  if (prevModal) {
+    prevModal.remove();
+    return;
+  }
+
+  const modal = document.createElement("div");
+  modal.className = "modal";
+  modal.classList.add("modal--open");
+
+  const backdrop = document.createElement("div");
+  backdrop.className = "modal-backdrop";
+
+  const container = document.createElement("div");
+  container.className = "modal-container";
+
+  const detailContainer = document.createElement("div");
+  detailContainer.className = "restaurant__detail";
+
+  const categoryDiv = document.createElement("div");
+  categoryDiv.className = "restaurant__category";
+  const image = document.createElement("img");
+  image.className = "category-icon";
+  image.src = restaurant.icon;
+  image.alt = restaurant.category;
+  categoryDiv.appendChild(image);
+
+  const infoDiv = document.createElement("div");
+  infoDiv.className = "restaurant__info";
+
+  const name = document.createElement("h3");
+  name.className = "restaurant__name text-subtitle";
+  name.textContent = restaurant.name;
+
+  const distance = document.createElement("span");
+  distance.className = "restaurant__distance text-body";
+  distance.textContent = `캠퍼스부터 ${restaurant.distance}분 내`;
+
+  const description = document.createElement("p");
+  description.className = "restaurant__description.no-clamp text-body";
+  description.textContent = restaurant.description;
+
+  const link = document.createElement("a");
+  link.textContent = restaurant.link;
+  link.href = restaurant.link;
+  link.target = "_blank";
+
+  infoDiv.append(name, distance, description, link);
+  detailContainer.append(categoryDiv, infoDiv);
+  container.appendChild(detailContainer);
+  modal.append(backdrop, container);
+  document.body.appendChild(modal);
 }
 
 addEventListener("load", main);
